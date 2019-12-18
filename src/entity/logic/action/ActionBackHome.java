743e36ba74571a431d2e7a2a7e0df82dea061807
Alban <alban.lacaille@hotmail.fr>
2019-12-18 07:30:22 +0100

    Pathfinding through borders

--- src/entity/logic/action/ActionBackHome.java
+++ src/entity/logic/action/ActionBackHome.java
@@ -2,7 +2,6 @@ package entity.logic.action;
 
 import entity.Ant;
 import openGL.utils.Maths;
-import openGL.world.Chunk;
 import openGL.world.World;
 import org.lwjgl.util.vector.Vector2f;
 import org.lwjgl.util.vector.Vector3f;
@@ -14,6 +13,8 @@ import java.util.Map;
 
 public class ActionBackHome extends Action {
 
+    private static final int A_STAR_WEIGHT_MULTIPLIER = 5;
+
     @Override
     public void execute(Ant a, World world) {
         Vector2f end = new Vector2f(0, 0);
@@ -21,12 +22,27 @@ public class ActionBackHome extends Action {
         if (path != null) {
             a.setTargetPosition(new Vector3f(path.x + .5f, 0, path.y + .5f));
         }
-        System.out.println("execute");
     }
 
-    private float h(Vector2f start, Vector2f end) {
-        //Todo account for looping through the borders
-        return (float) (5*Math.sqrt(Math.pow(start.x - end.x, 2) + Math.pow(start.y - end.y, 2)));
+    private float h(Vector2f start, Vector2f end, World world) {
+        float dist = Float.MAX_VALUE;
+        Vector2f[] loopedEnd = {
+                new Vector2f(end.x, end.y),
+                new Vector2f(end.x + world.getSizeX(), end.y),
+                new Vector2f(end.x - world.getSizeX(), end.y),
+                new Vector2f(end.x, end.y + world.getSizeZ()),
+                new Vector2f(end.x, end.y - world.getSizeZ()),
+                new Vector2f(end.x + world.getSizeX(), end.y + world.getSizeZ()),
+                new Vector2f(end.x + world.getSizeX(), end.y - world.getSizeZ()),
+                new Vector2f(end.x - world.getSizeX(), end.y + world.getSizeZ()),
+                new Vector2f(end.x - world.getSizeX(), end.y - world.getSizeZ())
+        };
+        for(Vector2f potentialEnd : loopedEnd) {
+            float endDist = (float) Math.sqrt(Math.pow(start.x - potentialEnd.x, 2) + Math.pow(start.y - potentialEnd.y, 2));
+            if (endDist < dist)
+                dist = endDist;
+        }
+        return A_STAR_WEIGHT_MULTIPLIER * dist;
     }
 
     private float getValue(Map<Vector2f, Float> map, Vector2f vec) {
@@ -42,7 +58,7 @@ public class ActionBackHome extends Action {
         Map<Vector2f, Float> fScore = new HashMap<>();
 
         gScore.put(start, 0f);
-        fScore.put(start, (h(start, end)));
+        fScore.put(start, (h(start, end, world)));
 
         while(openList.size() > 0) {
             Vector2f current = openList.get(0);
@@ -74,7 +90,7 @@ public class ActionBackHome extends Action {
 
                     cameFrom.put(vec, current);
                     gScore.put(vec, newGScore);
-                    fScore.put(vec, newGScore + h(vec, end));
+                    fScore.put(vec, newGScore + h(vec, end, world));
                     if (!openList.contains(vec)) {
                         openList.add(vec);
                     }
